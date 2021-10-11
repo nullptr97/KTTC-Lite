6ea89ba4179a689e506bf659ea86a4769b374ae7
Ярослав Стрельников <mr.strelnikov9@gmail.com>
2021-10-11 23:01:28 +0500

    Initial Commit

---
+++ WoT Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WoT Manager
+//
+//  Created by Ярослав Стрельников on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
